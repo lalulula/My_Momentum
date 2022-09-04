871813abc3242fde89c7fdf55d5647690558257f
Yunah Kim <yunah.kim@stonybrook.edu>
2022-09-04 19:27:26 +0900

    added current user to local storage

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 // const loginInput = loginForm.querySelector("input");
 // const loginButton= loginForm.querySelector("button");
 // const loginButton= document.querySelector("#login-form button");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const loginForm = document.getElementById("login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
-const HIDDEN_CLASSNAME = "hidden";
 
+if(savedUserName === null){
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener('submit',onLoginSubmit);
+}else{
+   paintGreetings(savedUserName)
+}
+
+function paintGreetings(username){
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+   greeting.innerText = `Hello ${username}`;
+}
 
 function onLoginSubmit(e){
    const username = loginInput.value;
    console.log(username);
    e.preventDefault();
+   localStorage.setItem(USERNAME_KEY, username);
    loginForm.classList.add(HIDDEN_CLASSNAME);
-   greeting.innerText = `Hello ${username}`;
-   greeting.classList.remove(HIDDEN_CLASSNAME);
+   paintGreetings(username)
 }
 
 
-loginForm.addEventListener('submit',onLoginSubmit);
+
