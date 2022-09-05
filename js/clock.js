b5e41767ad6e5890488ebbe621137dfd91b3c158
Yunah Kim <yunah.kim@stonybrook.edu>
2022-09-05 20:14:26 +0900

    added clock feature(no css)

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,11 @@
 const clock = document.getElementById("clock");
+
+function getClock(){
+  const date = new Date();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute =  String(date.getMinutes()).padStart(2, "0");
+  const seconds =String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hour}:${minute}:${seconds}`;
+};
+getClock();
+setInterval(getClock, 1000);
