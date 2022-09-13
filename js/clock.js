49359273a96707b18e0101607e2d7b62c43e9463
Yunah Kim <yunah.kim@stonybrook.edu>
2022-09-13 14:58:03 +0900

    fixed README & clock issues

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,8 @@
 const clock = document.getElementById("clock");
 const dateToday = document.getElementById("date");
-const date = new Date();
 
 function getDate(){
+  const date = new Date();
   let month = String(date.getUTCMonth()+1).padStart(2,"0");
   let day = String(date.getUTCDate()).padStart(2,"0");
   let year = String(date.getUTCFullYear()).padStart(2,"0");
@@ -10,6 +10,7 @@ function getDate(){
 
 }
 function getClock(){
+  const date = new Date();
   const hour = String(date.getHours()).padStart(2, "0");
   const minute =  String(date.getMinutes()).padStart(2, "0");
   const seconds =String(date.getSeconds()).padStart(2, "0");
@@ -17,6 +18,8 @@ function getClock(){
 };
 getDate();
 getClock();
+// setInterval(getClock, 1000);
 setInterval(function(){
-    getClock();
-    getDate();}, 1000);
+  getClock();
+  getDate();
+},1000)
